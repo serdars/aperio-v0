d8239a8de76ee3da3f0f94892c28c1eea9776998
serdars <serdarsutay@gmail.com>
2014-07-09 17:57:50 -0700

    Minor bug fixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id]) || current_user
   end
 
   # def edit
--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,5 @@
 module ConversationsHelper
+  def can_view_conversation?(conversation, user)
+    !conversation.group.private? || (user && conversation.group.member?(user))
+  end
 end
